cb9c38705efef6c66abc4fcff9c5a6a59b1beb79
tejascodex001 <tejas12gowda@gmail.com>
2026-01-02 20:38:51 +0530

    refactored the code and calculated cpu usage

--- sys_anamoly_detector/collector/src/main.cpp
+++ sys_anamoly_detector/collector/src/main.cpp
@@ -1,4 +1,6 @@
 #include <cstdint>
+#include <cstdio>
+#include <cstdlib>
 #include <iostream>
 #include <fstream>
 #include <sstream>
@@ -20,53 +22,32 @@ struct cpusnap {
     uint64_t softirq_time;
 };
 
-
-
-int main (){
-    long user, nice, system_kernel, idle, iowait, irq, softirq;
-    string label;
-    cpusnap cpuatt, cpuatt1;
+cpusnap read_cpu(){
+    cpusnap cpuatt;
     string folderpath = "/proc/stat";
-
     ifstream inputFile(folderpath);
-    if (inputFile.is_open()){
+    if(!inputFile.is_open()) { cout << "error not open" << endl; exit(0);}
+    else {
         string line;
-        getline(inputFile, line);
-        cout << line << endl;
+        getline(inputFile,line);
 
         istringstream(line) >> cpuatt.cpu_name >> cpuatt.user_time >> 
         cpuatt.nice_time >> cpuatt.system_kernel_time >> cpuatt.idle_time >> cpuatt.iowait_time >> 
         cpuatt.irq_time >> cpuatt.softirq_time;
+
         inputFile.close();
     }
+    return cpuatt;
+}
 
+int main (){  
+    cpusnap cpuatt, cpuatt1;
+    uint64_t total_delta, idle_delta, user_delta, nice_delta, system_delta, iowait_delta, irq_delta, softirq_delta;
+    double cpu_usage;
 
+    cpuatt = read_cpu();
     this_thread::sleep_for(1s);
-
-    ifstream inputFile1(folderpath);
-    if (inputFile1.is_open()){
-        string line;
-        getline(inputFile1, line);
-        cout << line << endl;
-        istringstream(line) >> cpuatt1.cpu_name >> cpuatt1.user_time >> 
-        cpuatt1.nice_time >> cpuatt1.system_kernel_time >> cpuatt1.idle_time >> cpuatt1.iowait_time >> 
-        cpuatt1.irq_time >> cpuatt1.softirq_time;
-
-        inputFile1.close();
-    }
-    else cout << "error not open" << endl;
-
-        cout << "\nCPU at time T" << endl;
-        cout <<  "cpu name: " << cpuatt.cpu_name << "\nuser_time: " << cpuatt.user_time <<
-        "\nnice_time:" << cpuatt.nice_time << "\nsystem_kern_time:" << cpuatt.system_kernel_time << "\nidle_time: " <<
-        cpuatt.idle_time << "\niowait_time: " << cpuatt.iowait_time << "\nirq_time: "
-        << cpuatt.irq_time << "\nsoftirq_time: " << cpuatt.softirq_time << endl;
-
-        cout << "\nCPU at time T1" << endl;
-         cout <<  "cpu name: " << cpuatt1.cpu_name <<"\nuser_time: " <<  cpuatt1.user_time << "\nnice_time:" <<
-        cpuatt1.nice_time << "\nsystem_kern_time:" << cpuatt1.system_kernel_time << "\nidle_time: " <<
-        cpuatt1.idle_time << "\niowait_time: " << cpuatt1.iowait_time << "\nirq_time: "
-        << cpuatt1.irq_time << "\nsoftirq_time: " << cpuatt1.softirq_time << endl;
+    cpuatt1 = read_cpu();
 
         cout << "\ndifference" << endl;
         cout << "user_time: " << cpuatt1.user_time - cpuatt.user_time << "\nnice_time:" <<
@@ -76,5 +57,17 @@ int main (){
         cpuatt1.iowait_time - cpuatt.iowait_time << "\nirq_time: " <<
         cpuatt1.irq_time - cpuatt.irq_time << "\nsoftirq_time: " <<
         cpuatt1.softirq_time - cpuatt.softirq_time << endl;
+
+    user_delta = cpuatt1.user_time - cpuatt.user_time;
+    nice_delta = cpuatt1.nice_time - cpuatt.nice_time;
+    system_delta = cpuatt1.system_kernel_time - cpuatt.system_kernel_time;
+    idle_delta = cpuatt1.idle_time - cpuatt.idle_time;
+    iowait_delta = cpuatt1.iowait_time - cpuatt.iowait_time;
+    irq_delta = cpuatt1.irq_time - cpuatt.irq_time;
+    softirq_delta = cpuatt1.softirq_time - cpuatt.softirq_time;
+
+    total_delta = user_delta + nice_delta + system_delta + idle_delta + iowait_delta + irq_delta + softirq_delta;
+    cpu_usage = double(total_delta - idle_delta)/double(total_delta);
+    cout << "\n" << cpu_usage*100 << "%" << endl;
         
 }
