05721be5dfca7bbcb9966b9e3a7bc49bae5341c3
tejascodex001 <tejas12gowda@gmail.com>
2026-01-07 22:52:03 +0530

    updated comment and some cleanup

--- collector/src/main.cpp
+++ collector/src/main.cpp
@@ -3,28 +3,27 @@
 #include "queue.hpp"
 #include <functional>
 #include <thread>
-#include <iostream>
 
 using namespace std;
 
 void write(Samplequeue &q){
     Writer writer("sample.csv");
-    std::cout << "writer is working" << endl;
     while(true){
         sample data = q.pop();
         writer.write_sample(data);
         writer.flush();
     }
 }
-
+/*  so writer.flush() was called as the csv file was not updating, now i need to implement 
+safe shutdown method, which I'll do tomorrow, I'm tired as i learnt many things
+    --Date: 7 Jan 2026.
+*/
 int main (){  
     Samplequeue samplequeue;
     thread t(write, ref(samplequeue));
-    cout << "main is working" << endl;
+
     while(true){
         sample data = sample_usage();
-        cout << "main while loop" << endl;
         samplequeue.push(data);
-        cout << "pushed" << endl;
     }
 }
